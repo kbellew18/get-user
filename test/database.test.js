3b147702f1fbbec6934ee330fb7eb433a943c8c0
Bellew Kieran <KieranBellew@BellewKeransMBP.lan>
2017-07-17 14:25:12 -0400

    slack test and app test working

--- test/database.test.js
+++ test/database.test.js
@@ -1,2 +1,30 @@
 var request = require('request');
 var assert = require('assert');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('127.0.0.1:27017/getUser');
+
+var app = require('../app');
+
+var collection = db.get('usercollection');
+
+describe('Database Test', function(){
+  it.skip('adds the correct item into the db', function(done){
+    collection.insert({
+      'firstname' : 'database',
+      'lastname' : 'test'
+    })
+
+    assert.equal('database', function(collection){
+      collection.find({})
+    });
+
+  });
+
+  it.skip('removes an item from the db', function(done){
+
+
+  });
+
+});
